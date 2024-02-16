5363ed41f3905b7b3f67be4e6f462f2d7d7da98b
Foullane55 <you@example.com>
2024-02-16 13:37:44 +0100

    twoSum

---
+++ twoSum.js
@@ -0,0 +1,25 @@
+// Given an array of integers nums and an integer target,
+// return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution,
+// and you may not use the same element twice.
+
+// You can return the answer in any order.
+//------------------------------------------
+// Example 1:
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+//solution//
+
+var twoSum = (nums, target)=>{
+  const obj = {}
+  for (let i = 0; i < nums.length; i++) {
+      const match = target - nums[i]
+      if (match in obj) return [i,obj[match]]
+         obj[nums[i]] = i
+}
+}
+
