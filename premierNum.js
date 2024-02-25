abf84925c001d0635d3a1107535f2fb1e0daef32
Foullane55 <118481724+Foullane55@users.noreply.github.com>
2024-02-25 14:28:05 +0100

    Create premierNum.js

---
+++ premierNum.js
@@ -0,0 +1,11 @@
+var prime =  (n) => {
+  let numbers = [2,3,5]
+  for (let i = 0; i < numbers.length; i++) {
+    if (n == numbers[i]) {
+      return true
+    }
+    if (n%numbers[i] == 0) {
+      return fals
+    }
+  }
+  }
